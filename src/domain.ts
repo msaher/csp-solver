efabe2e6d545e185b2ad785b02d1104285545e5d
saher <msaher.shair@gmail.com>
2023-04-12 21:07:51 +0300

    refactor: use an interface to allow is_in to be called without knowing the generic argument

--- src/domain.ts
+++ src/domain.ts
@@ -1,19 +1,23 @@
-export class Domain<T> {
-    private is_in: (x: T) => boolean;
+export interface Checker {
+    is_in(v: any): boolean;
+}
+
+export class Domain<T> implements Checker {
+    private _is_in: (x: T) => boolean;
 
     Domain(valid_values: Array<T> | ((x: T) => boolean)) {
         if (valid_values instanceof Array<T>) {
-            this.is_in = (x: T) => {
+            this._is_in = (x: T) => {
                 return valid_values.includes(x);
             };
         }
         else {
-            this.is_in = valid_values;
+            this._is_in = valid_values;
         }
     }
 
     // checks if the value x is in the domain
-    check(x: T): boolean {
-        return this.is_in(x);
+    is_in(x: T): boolean {
+        return this._is_in(x);
     }
 }
