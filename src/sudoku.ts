0ddd6bcc14d6812a415d14947fee9afd9a248447
saher <msaher.shair@gmail.com>
2023-04-14 15:45:55 +0300

    feat: Add sudoku assignment

--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,19 +1,6 @@
-import {Assignment} from './assignment'
+import {HomoAssign} from './assignment'
 
 export type Digit = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 export type Position = [Digit, Digit]
 
-class SudokuAssign implements Assignment<Position, Digit> {
-    map: Map<Position, Digit>
-    constructor(map: Map<Position, Digit>) {
-        this.map = map;
-    }
-
-    set(pos: Position, dig: Digit) {
-        return this.map.set(pos, dig);
-    }
-
-    get(pos: Position): Digit | undefined {
-        return this.map.get(pos);
-    }
-}
+export class SudokuAssign extends HomoAssign<Position, Digit> {}
