a6b1d1aa7cd5ecdef5d56469a7779715fda95b6d
saher <msaher.shair@gmail.com>
2023-04-14 15:44:41 +0300

    feat: add HomoAssignment

--- src/assignment.ts
+++ src/assignment.ts
@@ -1,4 +1,26 @@
-export interface Assignment<K, V> {
-    get(key: K): V | undefined;
-    set(key: K, value: V): void;
+export interface Assignment {
+    get(key: any): any;
+    set(key: any, value: any): void;
+}
+
+export class HomoAssign<K, V> implements Assignment {
+    map: Map<K, V>;
+
+    constructor(map: Map<K, V>) {
+        this.map = map;
+    }
+
+    get(key: K): V | undefined {
+        return this.map.get(key);
+    }
+
+    set(key: K, value: V): void {
+        this.map.set(key, value);
+    }
+
+}
+
+export interface Assignment {
+    get(key: any): any;
+    set(key: any, value: any): void;
 }
