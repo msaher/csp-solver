81f64e008869effe66f696536e23e4319f1d7dfd
saher <msaher.shair@gmail.com>
2023-04-14 14:56:54 +0300

    refactor: turn Assignment into an interface

--- src/assignment.ts
+++ src/assignment.ts
@@ -1,25 +1,4 @@
-import {Domain, Checker} from './domain';
-
-export class Assignment<K, V> {
-    domains: Map<K, Checker>;
-    map: Map<K, V>;
-
-    constructor(domains: Map<K, Domain<V>>) {
-        this.domains = domains;
-        this.map = new Map<K, V>();
-    }
-
-    get(k: K): V | undefined {
-        return this.map.get(k);
-    }
-
-    set(k: K, v: V) {
-        let checker = this.domains.get(k);
-        if (checker === undefined)
-            throw new Error("No such variable " + k);
-        if (!checker.is_in(v))
-            throw new Error("Value not in the domain: " + v);
-
-        return this.map.set(k, v);
-    }
+export interface Assignment<K, V> {
+    get(key: K): V | undefined;
+    set(key: K, value: V): void;
 }
