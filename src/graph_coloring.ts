c6fbb3cea6f49db9f690e91c3054bf98964725a9
saher <msaher.shair@gmail.com>
2023-04-14 15:46:07 +0300

    feat: add graph coloring assignment

--- src/graph_coloring.ts
+++ src/graph_coloring.ts
@@ -1,20 +1,6 @@
-import {Assignment} from './assignment'
+import {HomoAssign} from './assignment'
 
 export type Color = "R" | "G" | "B";
 export type Region = "WA" | "NT" | "SA" | "Q" | "NSW" | "V" | "T";
 
-export class AusterliaAssign implements Assignment<Region, Color> {
-    map: Map<Region, Color>
-
-    constructor(map: Map<Region, Color>) {
-        this.map = map;
-    }
-
-    set(reg: Region, col: Color) {
-        return this.map.set(reg, col);
-    }
-
-    get(reg: Region): Color | undefined {
-        return this.map.get(reg);
-    }
-}
+export class AusterliaAssign extends HomoAssign<Region, Color> {}
