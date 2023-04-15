7fcc55af8d408da96432e79e538c1790e32c959a
saher <msaher.shair@gmail.com>
2023-04-15 07:55:04 +0300

    chore: fix format

--- src/sudoku.ts
+++ src/sudoku.ts
@@ -1,6 +1,6 @@
 import {HomoAssign} from './assignment'
 
 export type Digit = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-export type Position = [Digit, Digit]
+export type Position = [Digit, Digit];
 
 export class SudokuAssign extends HomoAssign<Position, Digit> {}
