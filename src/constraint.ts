7d1cbefad0d4603b026b9cdf7f5e4a0332a00b03
saher <msaher.shair@gmail.com>
2023-04-17 11:16:36 +0300

    feat: add addConstraints

--- src/constraint.ts
+++ src/constraint.ts
@@ -10,7 +10,14 @@ function runChecks<K, V>(k1: K, v1: V, k2: K, v2: V, checkers: Checker<K, V>[]):
     return true;
 }
 
-class Constraints<K, V> {
+function wrap<T>(data: T | T[]): T[] {
+    if (data instanceof Array)
+        return data;
+    else
+        return [data];
+}
+
+export class Constraints<K, V> {
     graph: Graph<K, Checker<K, V>[]>;
 
     constructor(graph: Graph<K, Checker<K, V>[]>) {
@@ -19,7 +26,7 @@ class Constraints<K, V> {
 
     isConsistent(k1: K, v1: V, k2: K, v2: V): boolean {
         let edge = this.graph.getEdge(k1, k2);
-        if (edge === undefined)
+        if (edge === undefined) // no constraints between k1 and k2
             return true;
 
         let checkers = edge.weight;
@@ -29,7 +36,7 @@ class Constraints<K, V> {
     checkAssignment(assignment: Assignment<[K, V]>): boolean {
         for (const [k1, v1] of assignment.entries()) {
             let adj = this.graph.adjacency(k1);
-            if (adj === undefined)
+            if (adj === undefined) // k1 is isolated
                 continue;
             for (let edge of adj) {
                 let k2 = (edge.node1 === k1) ? edge.node2 : edge.node1;
@@ -39,5 +46,17 @@ class Constraints<K, V> {
             }
         }
         return true;
+    }
+
+    addConstraint(k1: K, k2: K, checks: Checker<K, V> | Checker<K, V>[]): Constraints<K , V> {
+        checks = wrap(checks);
+        let edge = this.graph.getEdge(k1, k2);
+        if (edge === undefined)
+            this.graph.addEdge(k1, k2, checks);
+        else
+            for (let c of checks)
+                edge.weight.push(c);
+
+        return this;
     }
 }
