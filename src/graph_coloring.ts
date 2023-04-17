d4af63fbb55a7f726827b45005b39f03be41b31d
saher <msaher.shair@gmail.com>
2023-04-17 11:59:48 +0300

    chore: export colors and regions

--- src/graph_coloring.ts
+++ src/graph_coloring.ts
@@ -1,9 +1,9 @@
 import {HashAssign} from './assignment'
 
-const colors = ["R", "G", "B"] as const;
+export const colors = ["R", "G", "B"] as const;
 export type Color = typeof colors[number];
 
-const regions = ["WA", "NT", "SA", "Q", "NSW", "V", "T"] as const;
+export const regions = ["WA", "NT", "SA", "Q", "NSW", "V", "T"] as const;
 export type Region = typeof regions[number];
 
 export class AusterliaAssign extends HashAssign<[Region, Color]> {}
